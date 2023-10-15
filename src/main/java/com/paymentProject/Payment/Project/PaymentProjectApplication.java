5578e1b1de0e4b88db7a0e062a58c4623ea06340
mittalyash394 <mittalyash394@gmail.com>
2023-10-15 15:43:12 +0530

    added swagger flow and change in github-action file

--- src/main/java/com/paymentProject/Payment/Project/PaymentProjectApplication.java
+++ src/main/java/com/paymentProject/Payment/Project/PaymentProjectApplication.java
@@ -2,12 +2,24 @@ package com.paymentProject.Payment.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PaymentProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentProjectApplication.class, args);
+	}
+
+	public Docket apis(){
+
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.paymentProject.Payment.Project")).build();
+
 	}
 
 }
--- src/main/java/com/paymentProject/Payment/Project/controller/PaymentController.java
+++ src/main/java/com/paymentProject/Payment/Project/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.paymentProject.Payment.Project.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,13 @@ public class PaymentController {
 
     public static final Logger log = LogManager.getLogger(PaymentController.class);
 
+    @Value("${spring.application.name}")
+    String applicationName;
+
     @GetMapping("/root")
     public String home(){
-        log.info("This is a home/root method");
-        return "This is a home URL";
+        log.info("This is a home root method");
+        return "This is a home URL of " + applicationName;
     }
 
 }
