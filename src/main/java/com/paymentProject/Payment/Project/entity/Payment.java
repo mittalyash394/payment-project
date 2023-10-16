417833310653dd1835b3ba92f13200b1a619f4ef
mittalyash394 <mittalyash394@gmail.com>
2023-10-16 23:20:34 +0530

    added the getter, setter and the constructors

--- src/main/java/com/paymentProject/Payment/Project/entity/Payment.java
+++ src/main/java/com/paymentProject/Payment/Project/entity/Payment.java
@@ -1,5 +1,14 @@
 package com.paymentProject.Payment.Project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
 public class Payment {
 
     private String paymentId;
--- src/main/java/com/paymentProject/Payment/Project/entity/Sender.java
+++ src/main/java/com/paymentProject/Payment/Project/entity/Sender.java
@@ -1,4 +1,13 @@
 package com.paymentProject.Payment.Project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
 public class Sender {
 }
